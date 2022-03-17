58b4447f36c4b29c695ff52463aeea1dacb34f7c
linzhaofeng <1552259734@qq.com>
2022-03-17 11:42:47 +0800

    全局配置和局部配置

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
 // app.js
-App({})
+App({
+    // 小程序初始化
+    onLaunch(options){
+        // console.log('onLaunch')
+
+        // console.log(options)
+        // switch(options.scene){
+        //     case 1005:
+        //         console.log("1005")
+        //         break
+        // }
+    },
+    // 小程序页面的显示
+    onShow(options){
+        // 获得用户信息
+        // wx.getUserInfo({
+        //   success(res){
+        //     console.log(res)
+        //   }
+        // })
+
+        // console.log('onShow')
+        // const err = new Error()
+        // throw err
+        // 判断小程序的进入场景
+        // switch(options.scene){
+        //     case 1012:
+        //         break
+        //     case 1013:
+        //         console.log("1013")
+        //         break
+        // }
+    },
+    // 小程序页面的隐藏
+    onHide(){
+        // console.log('onHide')
+    },
+    // 小程序的错误监听
+    onError(){
+        // console.log('onError')
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,42 @@
 // index.js
-Page({})
+Page({
+    data:{
+        message:'您好！谢先生',
+        movies:['三国演义','水浒传','红楼梦','西游记','海盗贼','鸣人传'],
+        userInfo:{}
+    },
+    // 页面加载
+    onLoad(){
+        console.log('页面onLoad')
+    },
+    // 页面显示
+    onShow(){
+        console.log('页面onShow')
+    },
+    // 页面初次渲染完成
+    onReady(){
+        console.log('页面onReady')
+    },
+    // 页面隐藏
+    onHide(){
+        console.log('页面onHide')
+    },
+    // 页面卸载
+    onUnload(){
+        console.log('页面onUnload')
+    },
+    handlegetUserInfo(event){
+        console.log(event)
+        this.setData({
+            userInfo:event.detail.rawData
+        })
+    },
+    // 监听用户下拉动作
+    onPullDownRefresh(){
+        console.log('上拉刷新')
+    },
+    // 上拉触底事件
+    onReachBottom(){
+        console.log('已经到达低部')
+    }
+})
